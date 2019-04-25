e53d4e7de9b0c3462a2f7a2397e7345ab31cbe5e
Kevin Tsai <k.tsai92@gmail.com>
2019-04-25 15:33:41 -0400

    Added some code.

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
+
 const apiKey= '335136-ShowFind-BZJT4Z3C'
 const searchURL = 'https://tastedive.com/api/similar'
 
 function formatParams(params){
    const queryItems = Object.keys(params)
-      .map(key => `${encodeURIcomponent(params)}=${encodeURIComponent(params[key])}`);
+      .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
       return queryItems.join('&');
 }
 
 function displayResults(responseJson) {
-	for (let i = 0 ; i < responseJson.Similar.length; i++){
+	for (let i = 0 ; i < responseJson.results.length; i++){
 		$('#results-list').append(`
-			${reponseJson.similar[i].results}`
-		)};
+			<li><p>${responseJson.results[i].name}</p></li>`
+      )};
+      $('#results').removeClass('hidden');
 } 
 
-function getShow(searchTerm){
+function getShow(searchTerm) {
     const params = {
-      q: searchTerm,
       k: apiKey,
+      q: searchTerm,
       info: 1,
 	 }
 	 
 	 const queryString = formatParams(params);
 	 const url = searchURL + '?' + queryString;
 
-   fetch(url)
+   fetch(url, {
+      mode: 'no-cors'
+   })
       .then(response => {
          if (response.ok) {
             return response.json();
@@ -40,8 +44,8 @@ function getShow(searchTerm){
 function watchForm() {
    $('form').submit(event => {
       event.preventDefault();
-		const searchTerm = $('#homeInput').val()
-		$('.container').empty()
+      const searchTerm = $('#homeInput').val();
+      $('form').empty()
 		getShow(searchTerm);
    });
 }
